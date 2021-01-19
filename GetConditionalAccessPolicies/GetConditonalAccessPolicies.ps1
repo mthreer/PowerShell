5f67a28a109ee35a9e63635fb3534bd4ffc018c4
Niklas Jumlin <mthreer@gmail.com>
2021-01-19 10:36:09 +0100

    GetConditionalAccessPolicies/GetConditonalAccessPolicies:

--- GetConditionalAccessPolicies/GetConditonalAccessPolicies.ps1
+++ GetConditionalAccessPolicies/GetConditonalAccessPolicies.ps1
@@ -59,7 +59,7 @@
 
 param (
     [CmdletBinding()]
-	[Parameter(Position=0,Mandatory=$false,HelpMessage="Output results as JSON to given file path")]
+    [Parameter(Position=0,Mandatory=$false,HelpMessage="Output results as JSON to given file path")]
     [ValidateScript({
         if(Test-Path $_ -PathType Container) {
             throw "The ExportToJson argument must be a file."
@@ -71,14 +71,14 @@ param (
         }
         return $true
     })]
-	[string]$ExportToJson
+    [string]$ExportToJson
 )
 
 Try { 
-	$null = Get-AzureADTenantDetail -ErrorAction Stop
+    $null = Get-AzureADTenantDetail -ErrorAction Stop
 } 
 Catch [Microsoft.Open.Azure.AD.CommonLibrary.AadNeedAuthenticationException] { 
-	Connect-AzureAD
+    Connect-AzureAD
 }
 
 $CAs=Get-AzureADMSConditionalAccessPolicy
@@ -101,9 +101,9 @@ function Test-IsGuid
 
 $global:PoliciesReport = [System.Collections.Generic.List[PSCustomObject]]@()
 foreach ($p in $CAs) {
-	$policy = [PSCustomObject]@{ 
-		Name = $p.DisplayName
-		Id = $p.Id
+    $policy = [PSCustomObject]@{ 
+        Name = $p.DisplayName
+        Id = $p.Id
         State = $p.State
         Users = [PSCustomObject]@{
             IncludedUsers = $([array]$a=$p.Conditions.Users.IncludeUsers;$(
@@ -132,22 +132,22 @@ foreach ($p in $CAs) {
                 }
                 else{$a}
                 ))
-			IncludedGroups = $([array]$a=$p.Conditions.Users.IncludeGroups;$a.ForEach{[PSCustomObject]@{$($_) = $((Get-AzureADObjectByObjectId -ObjectIds $_).DisplayName)}})
-			ExcludedGroups = $([array]$a=$p.Conditions.Users.ExcludeGroups;$a.ForEach{[PSCustomObject]@{$($_) = $((Get-AzureADObjectByObjectId -ObjectIds $_).DisplayName)}})
-			IncludedRoles = $([array]$a=$p.Conditions.Users.IncludeRoles;$a.ForEach{[PSCustomObject]@{$($_) = $($v=$_;$AADRoles.Where{$_.Id -eq $v}.DisplayName)}})
-			ExcludedRoles = $([array]$a=$p.Conditions.Users.ExcludeRoles;$a.ForEach{[PSCustomObject]@{$($_) = $($v=$_;$AADRoles.Where{$_.Id -eq $v}.DisplayName)}})
-		}
-		CloudAppOrUserActions = [PSCustomObject]@{
-			IncludedCloudApp = $([array]$a=$p.Conditions.Applications.IncludeApplications;if($a -ne "All"){$a.ForEach{$v=$_;$ServicePrincipals.Where{$_.AppId -eq $v}.DisplayName}}else{$a})
-			ExcludedCloudApp = $([array]$a=$p.Conditions.Applications.ExcludeApplications;$a.ForEach{$v=$_;$ServicePrincipals.Where{$_.AppId -eq $v}.DisplayName})
-			IncludedUserActions = $([array]$a=$p.Conditions.Applications.IncludeUserActions;if($a){$a -join ", "}else{$a})
-			IncludedProtectionLevels = $([array]$a=$p.Conditions.Applications.IncludedProtectionLevels;if($a){$a -join ", "}else{$a})
+            IncludedGroups = $([array]$a=$p.Conditions.Users.IncludeGroups;$a.ForEach{[PSCustomObject]@{$($_) = $((Get-AzureADObjectByObjectId -ObjectIds $_).DisplayName)}})
+            ExcludedGroups = $([array]$a=$p.Conditions.Users.ExcludeGroups;$a.ForEach{[PSCustomObject]@{$($_) = $((Get-AzureADObjectByObjectId -ObjectIds $_).DisplayName)}})
+            IncludedRoles = $([array]$a=$p.Conditions.Users.IncludeRoles;$a.ForEach{[PSCustomObject]@{$($_) = $($v=$_;$AADRoles.Where{$_.Id -eq $v}.DisplayName)}})
+            ExcludedRoles = $([array]$a=$p.Conditions.Users.ExcludeRoles;$a.ForEach{[PSCustomObject]@{$($_) = $($v=$_;$AADRoles.Where{$_.Id -eq $v}.DisplayName)}})
+        }
+        CloudAppOrUserActions = [PSCustomObject]@{
+            IncludedCloudApp = $([array]$a=$p.Conditions.Applications.IncludeApplications;if($a -ne "All"){$a.ForEach{$v=$_;$ServicePrincipals.Where{$_.AppId -eq $v}.DisplayName}}else{$a})
+            ExcludedCloudApp = $([array]$a=$p.Conditions.Applications.ExcludeApplications;$a.ForEach{$v=$_;$ServicePrincipals.Where{$_.AppId -eq $v}.DisplayName})
+            IncludedUserActions = $([array]$a=$p.Conditions.Applications.IncludeUserActions;if($a){$a -join ", "}else{$a})
+            IncludedProtectionLevels = $([array]$a=$p.Conditions.Applications.IncludedProtectionLevels;if($a){$a -join ", "}else{$a})
         }
         Conditions = [PSCustomObject]@{
-		    Platforms = [PSCustomObject]@{
+            Platforms = [PSCustomObject]@{
                 "Not yet implemented in the script" = $null
-		    }
-		    Locations = [PSCustomObject]@{
+            }
+            Locations = [PSCustomObject]@{
                 IncludedLocations = $([array]$a=$p.Conditions.Locations.IncludeLocations;if($a -ne "All"){$a.ForEach{
                     [PSCustomObject]@{
                         $((Get-AzureADMSNamedLocationPolicy -PolicyId $_).DisplayName) = $((Get-AzureADMSNamedLocationPolicy -PolicyId $_).Ipranges.CidrAddress)
@@ -158,7 +158,7 @@ foreach ($p in $CAs) {
                         $((Get-AzureADMSNamedLocationPolicy -PolicyId $_).DisplayName) = $((Get-AzureADMSNamedLocationPolicy -PolicyId $_).Ipranges.CidrAddress)
                     }
                     }}else{$a})
-		    }
+            }
             ClientAppTypes = @( 
                 $([string[]]$a=$p.Conditions.ClientAppTypes;$a.ForEach{$_})
             )
@@ -170,8 +170,8 @@ foreach ($p in $CAs) {
             Operator = $([string[]]$a=$p.GrantControls._Operator;$a.ForEach{$_})
             BuiltInControls = $([string[]]$a=$p.GrantControls.BuiltInControls;$a.ForEach{$_})
         }
-	}
-	$PoliciesReport.Add($policy)
+    }
+    $PoliciesReport.Add($policy)
 }
 if ($ExportToJson) {
     $PoliciesReport | ConvertTo-Json -Depth:99 | Out-File $ExportToJson -Encoding UTF8
