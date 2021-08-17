efdf1f975c166999dad572dd2eeddd6b703e9999
Niklas J. MacDowall <mthreer@gmail.com>
2021-08-17 11:30:29 +0200

    Update LogAppPoolState.ps1

--- LogAppPoolState.ps1
+++ LogAppPoolState.ps1
@@ -1,42 +1,57 @@
 #Requires -Modules WebAdministration
 #Requires -Version 5
 
+# Optional parameter input
 param (
     [CmdletBinding()]
     [Parameter(Position=0,Mandatory=$false,HelpMessage="Input IIS AppPool name to retrieve its state")]
     [string]$Name
 )
+# Import the IIS WebAdministration module
 Import-Module WebAdministration
 
 function LogAppPoolState($AppPoolName) {
+	# Try to retrieve the state
 	Try {
 		$State=Get-WebAppPoolState -Name "$AppPoolName" -ErrorAction Stop
 	}
 	Catch {
 		throw $_.Exception
 	}
+	# Check if state has a value and that it contains either Stopped or Started before continuing
 	if (($State.Value) -and ($State.Value -eq "Stopped" -or $State.Value -eq "Started")) {
 		Switch ($State.Value) {
-			"Stopped" { $EventId = "3001";$EntryType = "Warning" }
-			"Started" { $EventId = "3000";$EntryType = "Information" }
+			"Stopped" { $EventId = "3001";$EntryType = "Warning";[Switch]$WriteEventLog = $True }
+			"Started" { $EventId = "3000";$EntryType = "Information";[Switch]$WriteEventLog = $False }
 		}
-		Try { 
-			$Source = Get-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -ErrorAction Stop
+		#Write to EventLog if $WriteEventLog is True
+		if ($WriteEventLog) {
+			Try { 
+				# Check if the EventLog Source is already created
+				$Source = Get-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -ErrorAction Stop
+			}
+			Catch {
+				# Create the EventLog Source 
+				$null = New-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName"
+			}
+			# Write to the EventLog
+			Write-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -EventID $EventId -EntryType $EntryType -Message "IIS AppPool $AppPoolName state: $($State.Value)"
 		}
-		Catch {
-			$null = New-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName"
-		}
-		Write-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -EventID $EventId -EntryType $EntryType -Message "IIS AppPool $AppPoolName state: $($State.Value)"
 	}
 }
+# Add each AppPool Name to monitor
 $AppPoolNames = @(
 	"DefaultAppPool"
 )
+# Loop through array of AppPoolNames, if not parameter input is being used
 if (-not($Name)) {
 	ForEach ($AppPoolName in $AppPoolNames) { 
+		# execute the function LogAppPoolState that logs state to event log
 		LogAppPoolState -AppPoolName $AppPoolName
 	}
 }
+# If parameter input is being used
 if ($Name) {
+	# execute the function LogAppPoolState that logs state to event log
 	LogAppPoolState -AppPoolName $Name
 }
