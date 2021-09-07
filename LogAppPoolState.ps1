d029735145df32f4382a973cd639ae7606b4e40c
Niklas J. MacDowall <niklas.jumlin.macdowall@innofactor.com>
2021-09-07 16:16:04 +0200

    Added ScheduleOrchestrator.ps1, a PowerShell Workflow Runbook to handle execution of LogAppPoolState and LogWebSiteState on HybridWorkers

--- LogAppPoolState.ps1
+++ LogAppPoolState.ps1
@@ -12,45 +12,50 @@ param (
 Import-Module WebAdministration
 
 function LogAppPoolState($AppPoolName) {
-	# Try to retrieve the state
-	Try {
-		$State=Get-WebAppPoolState -Name "$AppPoolName" -ErrorAction Stop
-	}
-	Catch {
-		Write-Verbose $_.Exception
-		Continue;
-	}
-	# Check if state has a value and that it contains either Stopped or Started before continuing
-	if (($State.Value) -and ($State.Value -eq "Stopped" -or $State.Value -eq "Started")) {
-		Switch ($State.Value) {
-			"Stopped" { $EventId = "3001"; $EntryType = "Warning"; [Switch]$WriteEventLog = $True }
-			"Started" { $EventId = "3000"; $EntryType = "Information"; [Switch]$WriteEventLog = $False }
-		}
-		# Write to EventLog if $WriteEventLog is True
-		if ($WriteEventLog) {
-			Try { 
-				# Create the EventLog Source 
-				$null = New-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -ErrorAction Stop
-			}
-			Catch {}
-			# Write to the EventLog
-			Write-EventLog -LogName "Application" -Source "IIS AppPool $AppPoolName" -EventID $EventId -EntryType $EntryType -Message "IIS AppPool $AppPoolName state: $($State.Value)"
-		}
-	}
+    # Try to retrieve the state
+    Try {
+        $State=Get-WebAppPoolState -Name "$AppPoolName" -ErrorAction Stop
+    }
+    Catch {
+        Write-Verbose $_.Exception
+        Continue;
+    }
+    # Check if state has a value and that it contains either Stopped or Started before continuing
+    if (($State.Value) -and ($State.Value -eq "Stopped" -or $State.Value -eq "Started")) {
+        Switch ($State.Value) {
+            "Stopped" { $EventId = "3001"; $EntryType = "Warning"; [Switch]$WriteEventLog = $True }
+            "Started" { $EventId = "3000"; $EntryType = "Information"; [Switch]$WriteEventLog = $False }
+        }
+        # Log to Azure Automation Job Output
+        Write-Output "IIS AppPools `'$AppPoolName`' state: $($State.Value)"
+
+        # Write to EventLog if $WriteEventLog is True
+        if ($WriteEventLog) {
+            Try { 
+                # Check if the EventLog Source is already created
+                $null = New-EventLog -LogName "Application" -Source "IIS AppPool `'$AppPoolName`'" -ErrorAction Stop
+            }
+            Catch {
+                
+            }
+            # Write to the EventLog
+            Write-EventLog -LogName "Application" -Source "IIS AppPool `'$AppPoolName`'" -EventID $EventId -EntryType $EntryType -Message "IIS AppPool `'$AppPoolName`' state: $($State.Value)"
+        }
+    }
 }
 # Add each AppPool Name to monitor
 $AppPoolNames = @(
-	"DefaultAppPool"
+    "DefaultAppPool"
 )
 # Loop through array of AppPoolNames, if not parameter input $Name is being used
 if (-not($Name)) {
-	ForEach ($AppPoolName in $AppPoolNames) { 
-		# execute the function LogAppPoolState that logs state to event log
-		LogAppPoolState -AppPoolName $AppPoolName
-	}
+    ForEach ($AppPoolName in $AppPoolNames) { 
+        # execute the function LogAppPoolState that logs state to event log
+        LogAppPoolState -AppPoolName $AppPoolName
+    }
 }
 # If parameter input $Name is being used
 if ($Name) {
-	# execute the function LogAppPoolState that logs state to event log
-	LogAppPoolState -AppPoolName $Name
+    # execute the function LogAppPoolState that logs state to event log
+    LogAppPoolState -AppPoolName $Name
 }
--- LogWebSiteState.ps1
+++ LogWebSiteState.ps1
@@ -12,44 +12,50 @@ param (
 Import-Module WebAdministration
 
 function LogWebSiteState($WebSiteName) {
-	# Try to retrieve the state
-	Try {
-		$State=Get-WebSiteState -Name "$WebSiteName" -ErrorAction Stop
-	}
-	Catch {
-		Write-Verbose $_.Exception
-		Continue;
-	}
-	# Check if state has a value and that it contains either Stopped or Started before continuing
-	if (($State.Value) -and ($State.Value -eq "Stopped" -or $State.Value -eq "Started")) {
-		Switch ($State.Value) {
-			"Stopped" { $EventId = "4001"; $EntryType = "Warning"; [Switch]$WriteEventLog = $True }
-			"Started" { $EventId = "4000"; $EntryType = "Information"; [Switch]$WriteEventLog = $False }
-		}
-		# Write to EventLog if $WriteEventLog is True
-		if ($WriteEventLog) {
-			Try { 
-				# Create the EventLog Source 
-				$null = New-EventLog -LogName "Application" -Source "IIS WebSite $WebSiteName" -ErrorAction Stop
-			}
-			Catch {}
-			# Write to the EventLog
-			Write-EventLog -LogName "Application" -Source "IIS WebSite $WebSiteName" -EventID $EventId -EntryType $EntryType -Message "IIS WebSite $WebSiteName state: $($State.Value)"
-		}
-	}
+    # Try to retrieve the state
+    Try {
+        $State = Get-WebSiteState -Name "$WebSiteName" -ErrorAction Stop
+    }
+    Catch {
+        Write-Verbose $_.Exception
+        Continue;
+    }
+    # Check if state has a value and that it contains either Stopped or Started before continuing
+    if (($State.Value) -and ($State.Value -eq "Stopped" -or $State.Value -eq "Started")) {
+        Switch ($State.Value) {
+            "Stopped" { $EventId = "4001"; $EntryType = "Warning"; [Switch]$WriteEventLog = $True }
+            "Started" { $EventId = "4000"; $EntryType = "Information"; [Switch]$WriteEventLog = $False }
+        }
+        # Log to Azure Automation Job Output
+        Write-Output "IIS WebSite `'$WebSiteName`' state: $($State.Value)"
+        
+        # Write to EventLog if $WriteEventLog is True
+        if ($WriteEventLog) {
+            Try { 
+                # Check if the EventLog Source is already created
+                $null = New-EventLog -LogName "Application" -Source "IIS WebSite `'$WebSiteName`'" -ErrorAction Stop
+            }
+            Catch {
+
+            }
+            # Write to the EventLog
+            Write-EventLog -LogName "Application" -Source "IIS WebSite `'$WebSiteName`'" -EventID $EventId -EntryType $EntryType -Message "IIS WebSite `'$WebSiteName`' state: $($State.Value)"
+        }
+    }
 }
 # Add each WebSite Name to monitor
 $Sites = @(
-	"Default Web Site"
+    "Default Web Site"
 )
 # Loop through array of WebSiteNames, if not parameter input $Name is being used
 if (-not($Name)) {
-	ForEach ($Site in $Sites) { 
-		LogWebSiteState -WebSiteName $Site
-	}
+    ForEach ($Site in $Sites) { 
+        LogWebSiteState -WebSiteName $Site
+    }
 }
 # If parameter input $Name is being used
 if ($Name) {
-	# execute the function LogWebSiteState that logs state to event log
-	LogWebSiteState -WebSiteName $Name
+    # execute the function LogWebSiteState that logs state to event log
+    LogWebSiteState -WebSiteName $Name
 }
+
