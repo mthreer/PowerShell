e6969e920b0a5b482df720bef8aeb29c743c8e15
Niklas J. MacDowall <niklas.jumlin.macdowall@innofactor.com>
2021-09-08 14:45:24 +0200

    updated Orchestrator to be executed as a Watcher task instead

---
+++ Log-IIS-State/scripts/IISHybridWorkerOrchestrator.ps1
@@ -0,0 +1,57 @@
+Workflow IISHybridWorkerOrchestrator {
+    Param(
+        [Parameter(Mandatory=$True)]
+        [String]$HybridWorkerGroups
+    )
+
+    $HybridWorkerGroupsArray = $HybridWorkerGroups -split ","
+
+    $Runbooks = @(
+        "LogAppPoolState"
+        "LogWebsiteState"
+    )
+
+    ## Authentication
+    Write-Verbose "Logging into Azure ..."
+    Try {
+        # Ensures you do not inherit an AzContext in your runbook
+        $null = Disable-AzContextAutosave –Scope Process
+        # Connect to Azure using Managed Service Identity (MSI)
+        $null = Connect-AzAccount -Identity -SkipContextPopulation -ErrorAction Stop
+        # Verify that we have connected and that we have a context
+        $Conn = Get-AzContext
+        Write-Verbose "Successfully logged into Azure." 
+    } 
+    Catch {
+        if (-not($Conn)) {
+            $ErrorMessage = "Connection failed! A Managed Service Identity (MSI) could not be found."
+            throw $ErrorMessage
+        } 
+        else {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+    ## End of authentication
+    $null = Select-AzSubscription -SubscriptionId $Conn.Subscription
+
+    # Retrieve ResourceGroupName and AutomationAccountName to use as input to Start-AzAutomationRunbook
+    $ContextParameters = Get-AzAutomationAccount
+
+    ForEach -Parallel ($HybridWorkerGroup in $HybridWorkerGroupsArray) {
+        Write-Verbose "Waiting $timeoutInSeconds seconds ($interval minutes) before starting"
+        Start-Sleep -Seconds $timeoutInSeconds
+        $execution++
+        Write-Verbose "Running execution $execution/$executions"
+        foreach ($Runbook in $Runbooks) {
+            Try{
+                $job = Start-AzAutomationRunbook -Name "$Runbook" -RunOn "$HybridWorkerGroup" -ResourceGroupName "$($ContextParameters.ResourceGroupName)" -AutomationAccountName "$($ContextParameters.AutomationAccountName)"
+                Write-Output "Started parallel job `'$Runbook`' on `'$HybridWorkerGroup`': $($job.JobId)"
+            }
+            Catch {
+                Write-Error -Message $_.Exception.Message
+                throw $_.Exception
+            }
+        }
+    }
+}
